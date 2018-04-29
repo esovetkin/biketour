ba14cd2c8469a17eb6891d7d765ae905e703ad04
Jenya Sovetkin <e.sovetkin@gmail.com>
2018-04-29 20:09:30 +0200

    added quering historical weather

---
+++ historical_weather.py
@@ -0,0 +1,435 @@
+#!/bin/env python
+
+from darksky import forecast
+
+import sqlite3
+
+import logging
+
+from datetime import datetime, timedelta
+
+import random
+
+
+class Historical_Weather(object):
+    """A class queries historical weather at a list of coordinates
+
+    """
+
+    def __init__(self, coordinates, darksky_apikey,
+                 sample_size=20,
+                 sample_years=10,
+                 sample_around_interval=None,
+                 sample_current_date=None,
+                 darkskyapi_calls_limit=900,
+                 darksky_units = "si",
+                 filename="weather_history.db"):
+        """Initialise class
+
+        :coordinates: list of coordinates
+
+        :darksky_apikey: api key for darksky queries
+
+        :sample_size: number of days to query in the last sample_years
+
+        :sample_years: number of years in the past to query from
+
+        :sample_around_interval: number of days to query around which
+        the weather is queried (for example, when one is only
+        interested in summer days). In case None (default) the whole
+        year is sampled.
+
+        :sample_current_date: current date around which the
+        sample_around_interval is computed
+
+        :darkskyapi_calls_limit: maximum number of api calls allowed
+        to make per day
+
+        :darkskyapi_units: units to query darksky data (see more in darksky)
+
+        :filename: filename of the sqlite database to use that stores
+        the historical weather
+
+        """
+        self.coordinates=coordinates
+        self.apikey=darksky_apikey
+
+        self._sample_size=sample_size
+        self._sample_years=sample_years
+        self._sample_around_interval=sample_around_interval
+        self._sample_current_date=sample_current_date
+
+        self._darkskyapi_calls_limit = darkskyapi_calls_limit
+        self._darkskyapi_current_usage = None
+        self._darkskyapi_timestamp = None
+
+        self._darkskyapi_units = darksky_units
+
+        self.filename=filename
+
+        # initialise database connection
+        self._dbconn=sqlite3.connect(self.filename, timeout = 15,
+                                       isolation_level="EXCLUSIVE")
+
+        # initialise database
+        self._init_database()
+
+        # check that query dates exists
+        if 0 == self._count_query_dates_from_db():
+            days = self._sample_days_to_query()
+            self._save_query_dates_to_db(days)
+
+
+    def _init_database(self):
+        """This function initialised the database with weather data
+
+        """
+        try:
+            c = self._dbconn.execute('''BEGIN EXCLUSIVE''')
+
+            c.execute('''
+            CREATE TABLE IF NOT EXISTS weather
+            (
+            id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+            latitude            REAL NOT NULL,
+            longitude           REAL NOT NULL,
+            time                INTEGER NOT NULL,
+            summary             VARCHAR(125),
+            icon                VARCHAR(125),
+            precipIntensity     REAL,
+            precipProbability   REAL,
+            precipType          VARCHAR(125),
+            temperature         REAL,
+            apparentTemperature REAL,
+            dewPoint            REAL,
+            humidity            REAL,
+            pressure            REAL,
+            windSpeed           REAL,
+            windGust            REAL,
+            windBearing         REAL,
+            cloudCover          REAL,
+            uvIndex             REAL,
+            visibility          REAL,
+            ozone               REAL,
+            CONSTRAINT uc_time_latitude_longitude UNIQUE (time, latitude, longitude)
+            )''')
+
+            c.execute('''
+            CREATE TABLE IF NOT EXISTS query_dates
+            (
+            id                  INTEGER PRIMARY KEY AUTOINCREMENT,
+            time                INTEGER NOT NULL,
+            latitude            REAL NOT NULL,
+            longitude           REAL NOT NULL,
+            if_queried          INTEGER NOT NULL,
+            CONSTRAINT uc_time_latitude_longitude UNIQUE(time, latitude, longitude)
+            )''')
+
+            self._dbconn.commit()
+        except Exception as e:
+            logging.error("Error creating table (weather)",e)
+            self._dbconn.rollback()
+            raise e
+
+
+    def _sample_days_to_query(self):
+        """Generate table of the weather to query
+
+        """
+
+        # evaluate delta of the interval
+        if self._sample_around_interval is None:
+            delta=365
+        else:
+            delta=self._sample_around_interval/2
+        if delta>365:
+            delta=365
+
+        # get days ago
+        days_ago=set()
+        for k in range(0,self._sample_years):
+            days_ago=days_ago.union(range(365*(k+1)-delta,365*(k+1)+delta))
+        # remove last 2 weeks from the forecast
+        days_ago = days_ago.difference(range(0,14))
+
+        # sample from days_ago
+        days_ago = random.sample(days_ago, self._sample_size)
+
+        # evaluate date times
+        if self._sample_current_date is None:
+            self._sample_current_date=datetime.now()
+
+        # computed required timestamps
+        days=[(self._sample_current_date - timedelta(days=x)).strftime("%s") for x in days_ago]
+
+        # get a cross-product with coordinates
+        return [(x[0],x[1],y,False) for x in self.coordinates for y in days]
+
+
+    def _save_query_dates_to_db(self, days):
+        """Save list of days to query to the database
+
+        :days: sample of days
+        """
+
+        # get cursor
+        c = self._dbconn.cursor()
+
+        # insert values to the database
+        try:
+            c.executemany('''
+            INSERT OR REPLACE INTO query_dates
+            (latitude, longitude, time, if_queried)
+            VALUES (?,?,?,?)
+            ''', days)
+
+        except Exception as e:
+            logging.error("Error with db insertion into query_dates table",e)
+            self._dbconn.rollback()
+            raise e
+
+        # commit changes
+        self._dbconn.commit()
+
+
+    def _read_query_dates_from_db(self, if_queried=None):
+        """Read sample days from query_dates table
+
+        :if_queried: specifies which date select. If None select all
+        dates. If True select only those that are queried
+        (i.e. if_queried field is True). If False select only those
+        that are not yet queried.
+
+        """
+
+        # get cursor
+        c = self._dbconn.cursor()
+
+        # query data from the database
+        try:
+            if if_queried is None:
+                c.execute('''
+                SELECT time, latitude, longitude, if_queried
+                FROM query_dates
+                ''')
+            else:
+                c.execute('''
+                SELECT time, latitude, longitude, if_queried
+                FROM query_dates
+                WHERE if_queried = ?
+                ''', (bool(if_queried),))
+
+            query_res = c.fetchall()
+        except Exception as e:
+            logging.error("Error quering data from query_dates",e)
+            self._dbconn.rollback()
+            raise e
+
+        self._dbconn.commit()
+
+        return query_res
+
+
+    def _count_query_dates_from_db(self, if_queried=None):
+        """Count number of days in query_dates table
+
+        :if_queried: specifies which date select. If None select all
+        dates. If True select only those that are queried
+        (i.e. if_queried field is True). If False select only those
+        that are not yet queried.
+        """
+
+        # get cursor
+        c = self._dbconn.cursor()
+
+        # query data from the database
+        try:
+            if if_queried is None:
+                c.execute('''
+                SELECT count(*)
+                FROM query_dates
+                ''')
+            else:
+                c.execute('''
+                SELECT count(*)
+                FROM query_dates
+                WHERE if_queried = ?
+                ''', (bool(if_queried),))
+
+            query_res = c.fetchone()[0]
+        except Exception as e:
+            logging.error("Error quering data from query_dates",e)
+            self._dbconn.rollback()
+            raise e
+
+        self._dbconn.commit()
+
+        return query_res
+
+
+    def _isallowed_darksky(self):
+        """Check if the query is allowed
+
+        This operation uses locally known variables to decide wether
+        we can make another query
+        """
+        # TODO: to do not yet completely implemented
+
+        if self._darkskyapi_current_usage is not None and self._darkskyapi_calls_limit >= self._darkskyapi_calls_limit:
+            return False
+
+        return True
+
+
+    def _query_coordinate_time(self, coord, time):
+        """Make darksky query with at a given place and time
+
+        The query result is written to the database
+
+        :coord: coordinate of the place
+
+        :time: time in seconds since epoch
+
+        """
+        # try to make darklib query
+        try:
+            if not self._isallowed_darksky():
+                logging.error("Reached limit for darksky API calls")
+                raise RuntimeError('API limit')
+
+            logging.info("Quering time = " + str(time) + " coordinate = " + str(coord))
+
+            query_res = forecast(key=self.apikey,
+                                 latitude=coord[0],longitude=coord[1],
+                                 time=time, units=self._darkskyapi_units)
+
+            # TODO: verify here that result is not error
+
+            # update the API usage counters
+            self._darkskyapi_current_usage = query_res.response_headers['X-Forecast-API-Calls']
+            self._darkskyapi_timestamp = datetime.now()
+
+        except Exception as e:
+            logging.error("Error making query to darksky: ",e)
+            raise e
+
+        # lsit with new data
+        new_data=[]
+        # list with expected columns
+        expected_columns = ['time','summary','icon',
+                            'precipIntensity','precipProbability','precipType',
+                            'temperature','apparentTemperature',
+                            'dewPoint','humidity','pressure',
+                            'windSpeed','windGust','windBearing',
+                            'cloudCover','uvIndex','visibility','ozone']
+
+        for item in query_res['hourly']['data']:
+
+            # check presence of all columns in the query result
+            for col in expected_columns:
+                if col not in item.keys():
+                    item[col] = None
+
+            # add queried data to list
+            new_data += [(coord[0],coord[1],
+                          item['time'],item['summary'],item['icon'],
+                          item['precipIntensity'],item['precipProbability'],item['precipType'],
+                          item['temperature'],item['apparentTemperature'],
+                          item['dewPoint'],item['humidity'],item['pressure'],
+                          item['windSpeed'],item['windGust'],item['windBearing'],
+                          item['cloudCover'],item['uvIndex'],item['visibility'],item['ozone'])]
+
+        # get cursor
+        c = self._dbconn.cursor()
+
+        # insert values to the database
+        try:
+            # insert new weather data
+            c.executemany('''
+            INSERT OR REPLACE INTO weather
+            (latitude, longitude,
+            time, summary, icon,
+            precipIntensity, precipProbability, precipType,
+            temperature, apparentTemperature,
+            dewPoint, humidity, pressure,
+            windSpeed, windGust, windBearing,
+            cloudCover, uvIndex, visibility, ozone)
+            VALUES
+            (?,?,
+            ?,?,?,
+            ?,?,?,
+            ?,?,
+            ?,?,?,
+            ?,?,?,
+            ?,?,?,?)
+            ''', new_data)
+
+            # update the query_dates table
+            c.executemany('''
+            INSERT OR REPLACE INTO query_dates
+            (latitude, longitude, time, if_queried)
+            VALUES (?,?,?,?)
+            ''', [(coord[0],coord[1],time,True)])
+
+        except Exception as e:
+            logging.error("Error with db insertion into weather table",e)
+            self._dbconn.rollback()
+            raise e
+
+        # commit changes
+        self._dbconn.commit()
+
+    def query_darksky_weather(self):
+        """Query historical weather from darksky API at required time points
+
+        """
+        # read dates and coordinates to query
+        query_dates = self._read_query_dates_from_db(False)
+
+        for q in query_dates:
+            try:
+                self._query_coordinate_time((q[1],q[2]), q[0])
+            except Exception as e:
+                logging.warning("Error during queries",e)
+                break
+
+    def query_local_weather(self,columns=None,where=None):
+        """Query weather data from local database
+
+        :columns: columns to query (in sql language). If None then all
+        columns are queried
+
+        :where: where condition in terms of database column names and
+        sql language
+
+        """
+        # get cursor
+        c = self._dbconn.cursor()
+
+        if columns is None:
+            columns = '*'
+
+        # query data from the database
+        try:
+            if where is None:
+                c.execute('''
+                SELECT
+                ''' + columns + '''
+                FROM weather
+                ''')
+            else:
+                c.execute('''
+                SELECT
+                ''' + columns + '''
+                FROM weather
+                WHERE''' + where)
+
+            query_res = c.fetchall()
+
+        except Exception as e:
+            logging.error("Error quering data from query_dates",e)
+            self._dbconn.rollback()
+            raise e
+
+        return query_res
+
